e290735fc350c76ca390c4cda75cc3dd894b2379
chanderkumar22 <chanderk365@gmail.com>
2020-06-15 16:16:51 +0500

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,120 @@
+// === CHAPTER NO. 1 - TASK NO. 1 & 2 ===\\
+// alert("Error! Please enter a valid Password")
+
+// === CHAPTER NO. 1 - TASK NO. 3 ===\\
+// alert("Welcome to JS Land...\nHappy Coding!")
+
+// // === CHAPTER NO. 1 - TASK NO. 4 ===\\
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// // === CHAPTER NO. 1 - TASK NO. 5 ===\\
+// alert("Helow... i can run JS through my web brower's console");
+// save in console
+
+// // === CHAPTER NO. 2 - TASK NO. 1-4 ===\\
+// var uname = "Username";
+// var fullname = "Chander Kumar";
+//     var message = "Helow World";
+// alert(message);
+// var name = "Jhone Doe";
+// var age = "15 years old";
+// var com = "Certified in Mobile Application Development";
+// alert(name)
+// alert(age)
+// alert(com)
+
+// // === CHAPTER NO. 2 - TASK NO. 5 ===\\
+// var Pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(Pizza);
+
+// // === CHAPTER NO. 2 - TASK NO. 6 ===\\
+// var text = "My Email Address is "
+// var Email = "chanderk365@gmail.com"
+// alert(text+Email)
+
+// // === CHAPTER NO. 2 - TASK NO. 7 ===\\
+// var Book = "I am trying to learn from the book A smarter way to learn JavaScript"
+// alert(Book)
+
+// // === CHAPTER NO. 2 - TASK NO. 8 ===\\
+// var i = "Yah! I can write HTML content through JavaScript"
+// document.write(i)
+
+// // === CHAPTER NO. 2 - TASK NO. 9 ===\\
+// var dia = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(dia)
+
+// // === CHAPTER NO. 3 - TASK NO. 1 ===\\
+// alert("i am 20 years old")
+
+// // === CHAPTER NO. 3 - TASK NO. 2 ===\\
+// dont know about tag
+
+// // === CHAPTER NO. 3 - TASK NO. 3 ===\\
+// var a = "My birth year is 2000 <br> Data type of my ddeclared variable is number"
+// document.write(a)
+
+// // // === CHAPTER NO. 3 - TASK NO. 4 ===\\
+// var visit = prompt("Visitor's Name");
+// var Product = prompt("Product Title")
+// var Qty = prompt("Enter Quantity")
+// var a = (visit+ Qty + Product)
+// document.write(a+"purchase from XYZ Clothing")
+
+// // === CHAPTER NO. 3 - TASK NO. 5 ===\\
+// var a = 10, b=20, c=30;
+// var d = a+b+c;
+// document.write(d)
+
+// // === CHAPTER NO. 4 ===\\
+// legal & illegal variable names:
+// 1) A variable name can't contain any spaces.
+// 2) A variable name can contain only letters, numbers, dollar signs, and underscores.
+// 3) Though a variable name can't be any of JavaScript's keywords, it can contain keywords.
+// For example, userAlert and myVar are legal.
+// 4) Though a variable name can't be any of JavaScript's keywords, it can contain keywords.
+// For example, userAlert and myVar are legal.
+// 5) I teach the camelCase naming convention. Why "camelCase"? Because there is a hump or
+// two (or three) in the middle if the name is formed by more than one word. A camelCase
+// name begins in lower case. If there's more than one word in the name, each subsequent
+// word gets an initial cap, creating a hump. If you form a variable name with only one
+// word, like response, there's no hump. It's a camel that's out of food. Please adopt the
+// camelCase convention. It'll make your variable names more readable, and you'll be less
+// likely to get variable names mixed up.
+
+
+
+// // // === CHAPTER NO. 5 - TASK NO. 1 ===\\
+// var a = 3;
+// var b = 5;
+// var c = 3+5;
+// document.write("sum of", + a + "and" , + b +  "is" , c)
+
+// // === CHAPTER NO. 5 - TASK NO. 2 ===\\
+// var a = 3;
+// var b = 5;
+// var c = 5-3;
+// document.write("sum of", + a + "and" , + b +  "is" , c)
+
+// // === CHAPTER NO. 5 - TASK NO. 2 ===\\
+// var a = 3;
+// var b = 5;
+// var c = 5*3;
+// document.write("sum of", + a + "and" , + b +  "is" , c)
+
+// // === CHAPTER NO. 5 - TASK NO. 2 ===\\
+// var a = 3;
+// var b = 5;
+// var c = 3 % 5;
+// document.write("sum of", + a + "and" , + b +  "is" , c)
+
+
+
+
+ 
+
+
+
+
+
